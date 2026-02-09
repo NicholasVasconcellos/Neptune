c8621e3f0be630b5f4f3fb8f3ae6d6f2b5f457db
Nicholas Sily <Nicholas.svasc@gmail.com>
2026-02-09 17:32:17 -0500

    add home page with link to tabs

--- app/(protected)/(tabs)/index.jsx
+++ app/(protected)/(tabs)/index.jsx
@@ -1,9 +1,12 @@
-import { StyleSheet, View, useColorScheme } from "react-native";
+import { StyleSheet, View, Image, useColorScheme } from "react-native";
 import React from "react";
 import Card from "../../../components/Card";
+import Button from "../../../components/Button";
 import ThemedText from "../../../components/ThemedText";
 import { Colors } from "../../../Styles/Theme";
 import { useAuth } from "../../../context/AuthContext";
+import Logo from "../../../assets/NeptuneAppIcon.png";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
 const Home = () => {
   const theme = Colors[useColorScheme()] || Colors.light;
@@ -13,10 +16,49 @@ const Home = () => {
     <View style={[styles.container, { backgroundColor: theme.background }]}>
       <Card>
         <ThemedText style={styles.title}>Welcome to Neptune Swim</ThemedText>
-        <ThemedText>
-          Logged in as {session?.user?.email}
-        </ThemedText>
       </Card>
+      <Image source={Logo} style={styles.image} resizeMode="contain" />
+
+      <Card>
+        <ThemedText style={styles.title}>Welcome to Neptune Swim</ThemedText>
+        <ThemedText>Logged in as {session?.user?.email}</ThemedText>
+      </Card>
+
+      <Button
+        icon={
+          <FontAwesome6 name="add" size={24} color={theme.text} />
+        }
+        href={"/(protected)/(tabs)/addData"}
+      >
+        Log Data
+      </Button>
+
+      <Button
+        icon={
+          <FontAwesome6 name="chart-line" size={24} color={theme.text} />
+        }
+        href={"/(protected)/(tabs)/viewData"}
+      >
+        View Metrics
+      </Button>
+
+      <Button
+        icon={
+          <FontAwesome6 name="scroll" size={24} color={theme.text} />
+        }
+        href={"/(protected)/(tabs)/addTraining"}
+      >
+        Add New Training
+      </Button>
+
+      <Button
+        icon={
+          <FontAwesome6 name="play" size={24} color={theme.text} />
+        }
+        href={"/(protected)/(tabs)/viewTraining"}
+      >
+        Start Training
+      </Button>
     </View>
   );
 };
@@ -25,8 +67,10 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    alignSelf: "center",
-    alignItems: "stretch",
+    // Cross Axis:
+    // Center this child with the parent
+    alignSelf: "center", // center on Parent
+    alignItems: "stretch", // Stretch children to full width of parent
     flex: 1,
     gap: 4,
     justifyContent: "center",
@@ -34,6 +78,21 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: "bold",
     fontSize: 18,
-    marginBottom: 8,
+  },
+  card: {
+    backgroundColor: "#eee",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rba(0,0,0,0.1)",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    alignSelf: "center",
+    marginBottom: 20,
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
   },
 });
